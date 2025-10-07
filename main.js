7b51ab081dca7d2a5a0baa59d750fabb5e269c1c
joaopajee <162633168+joaopajee@users.noreply.github.com>
2025-10-07 14:30:29 -0300

    é a 30

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-
-
-
+ const caixaPrincipal = document.querySelector(".caixa-principal")
+ const caixaPerguntas = document.querySelector(".caixa-perguntas")
+ const caixaAlternativas = document.querySelector(".caixa-Alternativas")
+ const caixaResultado = document.querySelector(".caixa-Resultado")
+  const textoResultado = document.querySelector(".texto-Resultado")
 
 
 const perguntas = [
